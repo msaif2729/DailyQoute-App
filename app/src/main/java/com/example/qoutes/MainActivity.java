0385e2269914416acdbf96d2afb7e9b14f0ce2b6
msaif2729 <saiiifansari@gmail.com>
2024-07-11 22:21:46 +0530

    Last Commit

--- app/src/main/java/com/example/qoutes/MainActivity.java
+++ app/src/main/java/com/example/qoutes/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -140,6 +141,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the content to appear under the status bar so that the content
+        // doesn't resize when the status bar hides and shows.
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        // Make the status bar transparent
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
 
         txt = (TextView) findViewById(R.id.txt);
         txt1 = (TextView) findViewById(R.id.txt1);
@@ -362,6 +370,9 @@ public class MainActivity extends AppCompatActivity {
         if(theme.equals("purple")||theme.equals("pink")||theme.equals("cream")||theme.equals("yellow")||theme.equals("bottlegreen")||theme.equals("blue"))
         {
             imgresource = MainActivity.this.getResources().getIdentifier(theme,"color",getPackageName());
+            txt.setTextColor(MainActivity.this.getResources().getIdentifier("black","color",getPackageName()));
+            txt1.setTextColor(MainActivity.this.getResources().getIdentifier("black","color",getPackageName()));
+
         }
         else {
             imgresource = MainActivity.this.getResources().getIdentifier(theme,"drawable",getPackageName());
